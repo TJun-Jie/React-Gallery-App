ef1756111dec0a2c8ad78978176aebdae27ef7dd
TJun-Jie <tjunjie2000@gmail.com>
2020-07-24 17:34:32 +0800

    Added searchForm component

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
+import apiKey from './config';
+import SearchForm from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="Container">
-      <Nav /> 
-      <PhotoContainer />
-    </div>
-  );
+class App extends Component{
+
+  
+
+  render() {
+    return (
+      <div className="Container">
+        <SearchForm />
+        <Nav /> 
+        <PhotoContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
