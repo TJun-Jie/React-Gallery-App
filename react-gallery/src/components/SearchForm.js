bac60af1cb43df2188655048adb452dbf9d58d2b
TJun-Jie <tjunjie2000@gmail.com>
2020-08-07 02:40:09 +0800

    Fixed bug

--- react-gallery/src/components/SearchForm.js
+++ react-gallery/src/components/SearchForm.js
@@ -20,13 +20,15 @@ class SearchForm extends Component {
         this.props.handleSearch(this.state.value);
         // Redirect to the page
         this.props.history.push(`/search/${this.state.value}`);
-        document.getElementById('search').reset();
+        this.setState({
+            value: ''
+        })
 
     }
     render() {      
         return(
-            <form className="search-form" onSubmit={this.handleSubmit} action="/search">
-                <input type="search" value={this.state.value} onChange={this.handleValueChange} name="search" placeholder="Search" required/>
+            <form id="search" className="search-form" onSubmit={this.handleSubmit} action="/search">
+                <input type="search"  value={this.state.value} onChange={this.handleValueChange} name="search" placeholder="Search" required/>
                 <button to="/search" type="submit" className="search-button">
                     <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
